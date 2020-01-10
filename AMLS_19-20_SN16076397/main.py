80a053bdc275d453fb6b3024e80f09c99e3aae6f
Konrad Chlupka <chlupkaw@gmail.com>
2020-01-10 14:02:45 +0000

    Clean up main

--- AMLS_19-20_SN16076397/main.py
+++ AMLS_19-20_SN16076397/main.py
@@ -1,51 +1,53 @@
-# imports
+from A1.a1 import A1
 
-# ======================================================================================================================
-# Data preprocessing
-data_train, data_val, data_test = data_preprocessing(args...)
-
+# ======================================================================================
+# Data preprocessing - everything happens in files a1.py, a2.py, etc.
 
 
-# ======================================================================================================================
+# ======================================================================================
 # Task A1
+# Gender detection: male or female
 
-model_A1 = A1(args...)                 # Build model object.
-acc_A1_train = model_A1.train(args...) # Train model based on the training set (you should fine-tune your model based on validation set.)
-acc_A1_test = model_A1.test(args...)   # Test model based on the test set.
-Clean up memory/GPU etc...             # Some code to free memory if necessary.
+model_A1 = A1()
+acc_A1_train = model_A1.train()
+acc_A1_test = model_A1.test()
 
 
-# ======================================================================================================================
+# ======================================================================================
 # Task A2
-model_A2 = A2(args...)
-acc_A2_train = model_A2.train(args...)
-acc_A2_test = model_A2.test(args...)
-Clean up memory/GPU etc...
+# Emotion detection: smiling or not smiling
+model_A2 = A2()
+acc_A2_train = model_A2.train()
+acc_A2_test = model_A2.test()
 
 
-# ======================================================================================================================
+# ======================================================================================
 # Task B1
-model_B1 = B1(args...)
-acc_B1_train = model_B1.train(args...)
-acc_B1_test = model_B1.test(args...)
-Clean up memory/GPU etc...
+# Face shape recognition: 5 types of face shapes
+model_B1 = B1()
+acc_B1_train = model_B1.train()
+acc_B1_test = model_B1.test()
 
 
-# ======================================================================================================================
+# ======================================================================================
 # Task B2
-model_B2 = B2(args...)
-acc_B2_train = model_B2.train(args...)
-acc_B2_test = model_B2.test(args...)
-Clean up memory/GPU etc...
+# Eye color recognition: 5 types of eye colors
+model_B2 = B2()
+acc_B2_train = model_B2.train()
+acc_B2_test = model_B2.test()
 
 
-# ======================================================================================================================
+# ======================================================================================
 ## Print out your results with following format:
-print('TA1:{},{};TA2:{},{};TB1:{},{};TB2:{},{};'.format(acc_A1_train, acc_A1_test,
-                                                        acc_A2_train, acc_A2_test,
-                                                        acc_B1_train, acc_B1_test,
-                                                        acc_B2_train, acc_B2_test))
-
-# If you are not able to finish a task, fill the corresponding variable with 'TBD'. For example:
-# acc_A1_train = 'TBD'
-# acc_A1_test = 'TBD'
+print(
+    "TA1:{},{};TA2:{},{};TB1:{},{};TB2:{},{};".format(
+        acc_A1_train,
+        acc_A1_test,
+        acc_A2_train,
+        acc_A2_test,
+        acc_B1_train,
+        acc_B1_test,
+        acc_B2_train,
+        acc_B2_test,
+    )
+)
